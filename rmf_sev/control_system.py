8d0a1d41b5bb53e6394b82d387f6c40994a6e039
mm <engineer.pqm@gmail.com>
2024-09-17 01:00:04 +0700

    fix

--- rmf_sev/control_system.py
+++ rmf_sev/control_system.py
@@ -2,137 +2,76 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from robot_interfaces.msg import MissionTransport
-from robot_interfaces.srv import SearchStock
+from robot_interfaces.srv import SearchStock, CommandApi
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+
 from example_interfaces.srv import AddTwoInts
 from functools import partial
-from amd_sevtsv import ServerControl
+
+# from amd_sevtsv import ServerControl
 
 
-class Controlsystem(Node):
+class ControlsystemRMF(Node):
 
     def __init__(self):
-        super().__init__("minimal_publisher")
-        self.publisher_ = self.create_publisher(String, "topic", 10)
+        super().__init__("rmf_sever_control")
+        self.publisher_ = self.create_publisher(String, "demotest", 10)
         timer_period = 1  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-        self.mission_take_cart_empty = self.create_subscription(
-            MissionTransport, "transport_empty_cart", self.cart_empty_callback, 10
+        self.client_command = self.create_client(
+            CommandApi, "update_standard_robot_status"
         )
-        self.mission_take_cart_empty  # prevent unused variable warning
-        self.cli_empty_cart = self.create_client(SearchStock, "search_empty_cart")
-        self.cli_2point = self.create_client(AddTwoInts, "add_two_ints")
-        self._stock_req = SearchStock.Request()
+        self.timer_cb = MutuallyExclusiveCallbackGroup()
+        self.timer = self.create_timer(
+            2.0, self.main_loop, callback_group=self.timer_cb
+        )
 
-    def send_request(self):
-        # client = node.create_client(AddTwoInts, "add_two_ints")
-        # while not self.cli_2point.wait_for_service(1.0):
-        #     self.get_logger().warn("Waiting for Server Add Two Ints...")
-        #     return True
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+        self._command_req = CommandApi.Request()
 
-        # request = AddTwoInts.Request()
-        # request.a = 3
-        # request.b = 8
-        mx = ServerControl.searching_information_db("find_cart_empty6")
-        # self.get_logger().info(mx)
-
-        # future.result()
-
-    def callback_call_add_two_ints(self, future):
-        try:
-            response = future.result()
-            self.get_logger().info(str(response.sum))
-            # if response is not None:
-            # self.hhhmmm(response)
-            # hh = "asdasdasda"
-            return response
-        except Exception as e:
-            self.get_logger().error("Service call failed %r" % (e,))
-        return response
-
-    def hhhmmm(self, respon):
-        self.get_logger().info(str(respon.sum))
-
-    def cart_empty_callback(self, msg):
-        self.get_logger().info("service not available, waiting again...")
-        # self.send_request()
-        if len(msg.mission_excute) < 4:
-            # self.find_cart_empty()
-            #  request mission
-            self.get_logger().info('I heard: "%s"' % msg.mission_type)
-
-    def send_requestssss(self):
-        # req = SearchStock.Request()
-        while not self.cli_empty_cart.wait_for_service(timeout_sec=1.0):
+    def find_location_infor_client(self):
+        while not self.client_command.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("service not available, waiting again...")
             return False
 
-        self._stock_req.url = "find_cart_empty6"
-        self.get_logger().info(", waiting again...")
-        return self.cli_empty_cart.call_async(self._stock_req)
+        mx = {"map_code": "pickup_locations", "location_status": 3}
+        self.get_logger().info('response_api: "%s"' % mx)
+        self._command_req.msg_request = str(mx)
+        future = self.client_command.call_async(self._command_req)
+        while rclpy.ok():
+            if future.done() and future.result():
+                return future.result()
 
-    def find_cart_empty(self):
-        future = self.send_requestssss()
-        self.get_logger().info(" future")
-        rclpy.spin_until_future_complete(self, future)
-        self.get_logger().info(", waiting again...")
+        return None
 
-        try:
-            response = future.result()
-            self.get_logger().info(str(response))
-            # if response is not None:
-            return_body = {"name": "Asdasdasd"}
-            return return_body
-        except Exception as e:
-            self.get_logger().error("Service call failed %r" % (e,))
-        return response
-
-        # req = SearchStock.Request()
-        # while not self.cli_empty_cart.wait_for_service(timeout_sec=1.0):
-        #     self.get_logger().info("service not available, waiting again...")
-        #     return False
-
-        # req.url = "find_cart_empty6"
-        # self.get_logger().info(", waiting again...")
-        # future = self.cli_empty_cart.call_async(req)
-        # while rclpy.ok():
-        #     # self.get_logger().info(future.done() )
-        #     if future.done() and future.result():
-        #         self.get_logger().info(future.result())
-        #         # return self.future.result()
-
-        # # hh = future.add_done_callback(partial(self.find_empty_cart_callback))
-        # self.get_logger().info(("axsdas"))
-
-    def find_empty_cart_callback(self, future):
-        return_body = {}
-        try:
-            response = future.result()
-            self.get_logger().info(str(response))
-            # if response is not None:
-            return_body = {"name": "Asdasdasd"}
-            return return_body
-        except Exception as e:
-            self.get_logger().error("Service call failed %r" % (e,))
-        return response
-
-    def timer_callback(self):
+    def main_loop(self):
         msg = String()
-        self.send_request()
+        # self.send_request()
+        request_body = {
+            "location_code": "zone9",
+            "map_code": "return_locations",
+            "excute_code": "transport_goods",
+        }
+
+        test = self.find_location_infor_client()
         # self.find_cart_empty()
-        msg.data = "Hello World: %d" % self.i
+        # self.get_logger().info('response_api: "%s"' % test)
+
+        msg.data = str(request_body)
         self.publisher_.publish(msg)
-        # self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
+        self.get_logger().info(
+            'query_mission_take_cart_empty: "%s"' % "minhsieudeptrai"
+        )
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = Controlsystem()
+    rmf_sever_control = ControlsystemRMF()
 
-    rclpy.spin(minimal_publisher)
-    minimal_publisher.destroy_node()
+    rclpy.spin(rmf_sever_control)
+    rmf_sever_control.destroy_node()
     rclpy.shutdown()
 
 
--- rmf_sev/mission_req.py
+++ rmf_sev/mission_req.py
@@ -15,15 +15,15 @@ from rclpy.node import Node
 
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
-from robot_interfaces.srv import CreatMission, SearchStock, ExcuteMission
-from robot_interfaces.msg import MissionTransport
+from robot_interfaces.srv import CreatMission, SearchStock, ExcuteMission, CommandApi
+from robot_interfaces.msg import MissionTransport, MissionCurrent
 
 
 app = FastAPI(
-    title="FastAPI JWT",
+    title="Robot API",
     openapi_url="/openapi.json",
     docs_url="/docs",
-    description="fastapi jwt",
+    description="controlsystem",
 )
 
 
@@ -41,12 +41,15 @@ class MissionRequestClient(Node):
     def __init__(self):
         super().__init__("mission_process_client_async")
         self.cb = None
-        self.get_logger().info("Swagger run http://127.0.0.1:5000/docs#/")
+        self.get_logger().info("Swagger run http://127.0.0.1:2000/docs#/")
 
         self.timer_cb = MutuallyExclusiveCallbackGroup()
 
         self.cli_empty_cart = self.create_client(SearchStock, "search_empty_cart")
         self.cli_new_mission = self.create_client(CreatMission, "creation_mission")
+        self.cli_data_update_status = self.create_client(
+            CommandApi, "update_data_database"
+        )
         self.cli_excute_mission = self.create_client(
             ExcuteMission, "processing_excute_mission"
         )
@@ -58,10 +61,20 @@ class MissionRequestClient(Node):
             5.0, self.main_loop, callback_group=self.timer_cb
         )
 
+        self.subscription_mission_current = self.create_subscription(
+            MissionCurrent,
+            "mission_current_request",
+            self.mission_current_callback,
+            10,
+        )
+
         self.mission_take_cart_empty = self.create_subscription(
             MissionTransport, "transport_empty_cart", self.cart_empty_callback, 10
         )
+
         self.query_mission_take_cart_empty = False
+        self.mission_transport_goods_current = None
+        self.mission_transport_empty_cart_current = None
 
         @app.post("/occupy_location")
         async def occupy_locations(location_occupy: dict):
@@ -89,6 +102,35 @@ class MissionRequestClient(Node):
                 return mission_transport_goods
             else:
                 return "Error call excute_code "
+
+        @app.post("/update_robot_status")
+        async def update_robot_status(robot_update: dict):
+            self.get_logger().info('robot_update: "%s"' % robot_update)
+            url_name = "update_robotStatus"
+            data_return = self.processing_update_client(str(url_name), robot_update)
+
+            return eval(data_return.msg_response)
+
+        @app.post("/robot_request_mission")
+        async def robot_request_mission(robot_request: dict):
+            # self.get_logger().info('robot_update: "%s"' % robot_update)
+            _excute_code = robot_request["excute_code"]
+            process_request = self.robot_processing_take_mission(_excute_code, 2)
+            return process_request
+
+        @app.post("/robot_comfirm_mission")
+        async def robot_comfirm_mission(robot_comfirm: dict):
+            process_mission = self.mission_runing_process(robot_comfirm)
+            self.get_logger().info('robot_update: "%s"' % process_mission)
+
+            return process_mission
+
+        @app.post("/robot_update_location")
+        async def robot_update_location(robot_comfirm: dict):
+            # process_mission = self.mission_runing_process(robot_comfirm)
+            # self.get_logger().info('robot_update: "%s"' % process_mission)
+
+            return robot_comfirm
 
     def mission_creat_transport_goods(self, end_location):
 
@@ -142,6 +184,21 @@ class MissionRequestClient(Node):
             mission_carry_empty_cart.mission_code,
         )
         return mission_carry_empty_cart.mission_code
+
+    def processing_update_client(self, _url, request_body):
+        req = CommandApi.Request()
+        while not self.cli_data_update_status.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
+            return False
+
+        req.url = _url
+        req.msg_request = str(request_body)
+        future = self.cli_data_update_status.call_async(req)
+        while rclpy.ok():
+            if future.done() and future.result():
+                return future.result()
+
+        return None
 
     def find_location_infor_client(self, _url):
         while not self.cli_empty_cart.wait_for_service(timeout_sec=1.0):
@@ -205,7 +262,7 @@ class MissionRequestClient(Node):
             new_mission_code = self.creat_mission_take_empty_cart()
             self.get_logger().info(str(new_mission_code))
 
-        self.get_logger().info("loop run")
+        # self.get_logger().info("loop run")
 
     def creat_mission_take_empty_cart(self):
         response_empty_cart = self.find_location_infor_client("find_cart_empty/6")
@@ -235,6 +292,60 @@ class MissionRequestClient(Node):
         )
         return new_mission_code.mission_code
 
+    def mission_runing_process(self, request):
+        url_name = "remove_pending_task"
+        data_return = self.processing_update_client(str(url_name), request)
+        return eval(data_return.msg_response)
+
+    def robot_processing_take_mission(self, _excute_code, n):
+        # _excute_code = request_robot["excute_code"]
+        if n == 0:
+            return {"code": "asdasd"}
+
+        _map_code = ["transport_empty_cart", "transport_goods"]
+        mission_infor = ""
+        if _excute_code == "transport_empty_cart":
+            mission_infor = self.mission_transport_empty_cart_current
+            # mission_infor = None
+        elif _excute_code == "transport_goods":
+            # mission_infor = None
+            mission_infor = self.mission_transport_goods_current
+
+        # mission_infor = None
+        # url_name = "missions_excute_pop"
+        # _location = {"excute_code": _excute_code}
+        # data_return = self.processing_update_client(str(url_name), _location)
+        if mission_infor is None:
+            _map_code.remove(_excute_code)
+            return self.robot_processing_take_mission(_map_code[0], n - 1)
+            # self.get_logger().info('request_robot: "%s"' % (_map_code[0]))
+        # self.get_logger().info('mission_infor: "%s"' % (mission_infor))
+        # self.get_logger().info('_excute_code: "%s"' % (_excute_code))
+        # self.get_logger().info('n: "%s"' % (n))
+
+        _location = {"excute_code": _excute_code}
+        url_name = "missions_excute_pop"
+        data_return = self.processing_update_client(str(url_name), _location)
+        return mission_infor
+
+    def mission_current_callback(self, msg):
+        _mission_transport_goods_current = eval(msg.transport_goods)
+        _mission_transport_empty_cart_current = eval(msg.transport_empty_cart)
+        if _mission_transport_goods_current["code"]:
+            self.mission_transport_goods_current = {
+                "entry_location": _mission_transport_goods_current["entry_location"],
+                "end_location": _mission_transport_goods_current["end_location"],
+                "excute_code": "transport_goods",
+            }
+        self.mission_transport_empty_cart_current = {
+            "entry_location": _mission_transport_empty_cart_current["entry_location"],
+            "end_location": _mission_transport_empty_cart_current["end_location"],
+            "excute_code": "transport_empty_cart",
+        }
+        # self.get_logger().info(
+        #     'request_body: "%s"' % self.mission_transport_goods_current
+        # )
+
     def cart_empty_callback(self, msg):
         # self.get_logger().info("hello on here")
 
@@ -258,8 +369,8 @@ def main(args=None):
     # executor.spin()
     spin_thread = threading.Thread(target=executor.spin, daemon=True)
     spin_thread.start()
-
-    uvicorn.run(app, port=5000, log_level="warning")
+    uvicorn.run(app, port=2000, log_level="warning")
+    minimal_client.destroy_node()
 
     rclpy.shutdown()
 
