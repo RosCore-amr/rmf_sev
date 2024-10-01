9225b744e88364303dee94560770a0afb87ba59f
mm <engineer.pqm@gmail.com>
2024-10-01 14:16:11 +0700

    fix update robot and mission current

--- rmf_sev/mission_req.py
+++ rmf_sev/mission_req.py
@@ -13,6 +13,7 @@ import threading
 from example_interfaces.srv import AddTwoInts
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
@@ -25,6 +26,7 @@ from robot_interfaces.srv import (
     GetInformation,
 )
 from robot_interfaces.msg import MissionTransport, MissionCurrent
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -32,6 +34,15 @@ app = FastAPI(
     openapi_url="/openapi.json",
     docs_url="/docs",
     description="controlsystem",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -71,7 +82,7 @@ class MissionRequestClient(Node):
         )
 
         self.subscription_mission_current = self.create_subscription(
-            MissionCurrent,
+            String,
             "mission_current_request",
             self.mission_current_callback,
             10,
@@ -94,16 +105,20 @@ class MissionRequestClient(Node):
             # 3 co xe, co hang
             # 5 vi tri trong
             # self.get_logger().info(str(empty_location))
+
+            _occupy_location = self.find_location_infor_client(
+                str("query_location/" + "return_locations/" + location_occupy["name"])
+            )
+            _location = eval(_occupy_location.msg_response)
+            if not _location:
+                return {"mission_code": 0, "msg": "Call error"}
+
             if location_occupy["excute_code"] == "transport_empty_cart":
-                mission_empty_cart = self.mission_creat_transport_empty_cart(
-                    location_occupy
-                )
+                mission_empty_cart = self.mission_creat_transport_empty_cart(_location)
                 self.get_logger().info('mission_empty_cart: "%s"' % mission_empty_cart)
                 return mission_empty_cart
             elif location_occupy["excute_code"] == "transport_goods":
-                mission_transport_goods = self.mission_creat_transport_goods(
-                    location_occupy
-                )
+                mission_transport_goods = self.mission_creat_transport_goods(_location)
                 self.get_logger().info(
                     'mission_transport_goods: "%s"' % mission_transport_goods
                 )
@@ -112,18 +127,40 @@ class MissionRequestClient(Node):
                 return "Error call excute_code "
 
         @app.post("/update_robot_status")
-        async def update_robot_status(robot_update: dict):
-            self.get_logger().info('robot_update: "%s"' % robot_update)
-            url_name = "update_robotStatus"
-            data_return = self.processing_update_client(str(url_name), robot_update)
+        async def update_robot_status(robot_update: dict, request: Request):
+            _ip_host = request.client.host
+            _ip = {"ip_machine": _ip_host}
+            robot_update.update(_ip)
 
-            return eval(data_return.msg_response)
+            element_update = robot_update.keys()
+            if (
+                "collision" not in element_update
+                and "robot_status" not in element_update
+            ):
+                return {"code": 0}
+            _return = {}
+            collision_process = {
+                "ip_machine": _ip_host,
+                "collision": robot_update["collision"],
+                "robot_status": robot_update["robot_status"],
+                "map_code": robot_update["map_code"],
+            }
+            self.get_logger().info('collision_process: "%s"' % collision_process)
+            self.get_logger().info('element_update: "%s"' % element_update)
+
+            url_update_robot = "update_robotStatus"
+            data_return = self.processing_update_client(
+                str(url_update_robot), robot_update
+            )
+            _return.update(eval(data_return.msg_response))
+
+            return _return
 
         @app.post("/robot_request_mission")
         async def robot_request_mission(robot_request: dict):
             # self.get_logger().info('robot_update: "%s"' % robot_update)
-            _excute_code = robot_request["excute_code"]
-            process_request = self.robot_processing_take_mission(_excute_code, 2)
+            _map_code = robot_request["map_code"]
+            process_request = self.robot_processing_take_mission(_map_code, 2)
             return process_request
 
         @app.post("/robot_comfirm_mission")
@@ -142,7 +179,7 @@ class MissionRequestClient(Node):
             self.get_logger().info('client_host: "%s"' % client_host)
             return robot_comfirm
 
-        @app.post("/collision")
+        @app.post("/collision_request")
         async def collision(robot_comfirm: dict):
             # client_host = request.client.host
             # # process_mission = self.mission_runing_process(robot_comfirm)
@@ -151,17 +188,10 @@ class MissionRequestClient(Node):
 
             return eval(sever_response.result)
 
-    def mission_creat_transport_goods(self, end_location):
-
-        location_return = self.find_location_infor_client(
-            str("query_location/" + "return_locations/" + end_location["name"])
-        )
-        _location_return = eval(location_return.msg_response)
-        if not _location_return:
-            return {"mission_code": 0, "msg": "location not exist"}
+    def mission_creat_transport_goods(self, _location_return):
 
         location_goods = self.find_location_infor_client(
-            "find_products/" + str(end_location["name"])
+            "find_products/" + str(_location_return["name"])
         )
         _location_have_goods = eval(location_goods.msg_response)
         if not _location_have_goods:
@@ -169,6 +199,7 @@ class MissionRequestClient(Node):
 
         # mx = self.pre_location()
         # self.get_logger().info('location_return : "%s"' % location_return)
+        task_performed = "transport_goods"
 
         body_request = {
             "entry_location": {
@@ -181,12 +212,13 @@ class MissionRequestClient(Node):
                 "map_code": _location_return["map_code"],
                 "point": _location_return["point"],
             },
+            "excute_code": task_performed,
         }
 
         mission_carry_goods = self.creation_mission_client(body_request)
         self.process_excute_mission_client(
             "missions_excute_update",
-            end_location["excute_code"],
+            task_performed,
             mission_carry_goods.mission_code,
         )
         # self.get_logger().info('body_request: "%s"' % body_request)
@@ -194,13 +226,7 @@ class MissionRequestClient(Node):
         # return True
         return {"mission_code": mission_carry_goods.mission_code, "msg": 1}
 
-    def mission_creat_transport_empty_cart(self, entry_location):
-        empty_cart_location = self.find_location_infor_client(
-            str("query_location/" + "return_locations/" + entry_location["name"])
-        )
-        _empty_cart_location = eval(empty_cart_location.msg_response)
-        if not _empty_cart_location:
-            return {"mission_code": 0, "msg": "Call error"}
+    def mission_creat_transport_empty_cart(self, _empty_cart_location):
 
         available_location = self.find_location_infor_client(
             "available_location/pickup_locations"
@@ -210,6 +236,7 @@ class MissionRequestClient(Node):
             return {"mission_code": 0, "msg": "not have available location"}
 
         # _available_location = eval(available_location.msg_response)[0]
+        task_performed = "transport_empty_cart"
 
         body_request = {
             "entry_location": {
@@ -222,12 +249,13 @@ class MissionRequestClient(Node):
                 "map_code": _available_location["map_code"],
                 "point": _available_location["point"],
             },
+            "excute_code": task_performed,
         }
 
         mission_carry_empty_cart = self.creation_mission_client(body_request)
         self.process_excute_mission_client(
             "missions_excute_update",
-            "transport_empty_cart",
+            task_performed,
             mission_carry_empty_cart.mission_code,
         )
         return mission_carry_empty_cart.mission_code
@@ -327,7 +355,7 @@ class MissionRequestClient(Node):
 
         if self.query_mission_take_cart_empty:
             new_mission_code = self.creat_mission_take_empty_cart()
-            self.get_logger().info(str(new_mission_code))
+            # self.get_logger().info(str(new_mission_code))
 
             # self.get_logger().info("loop run")
 
@@ -351,53 +379,33 @@ class MissionRequestClient(Node):
         return eval(data_return.msg_response)
 
     def robot_processing_take_mission(self, _excute_code, n):
-        # _excute_code = request_robot["excute_code"]
-        if n == 0:
-            return {"code": "asdasd"}
 
-        _map_code = ["transport_empty_cart", "transport_goods"]
-        mission_infor = ""
+        if n == 0:
+            return {"mission_code": 0, "code": "dont have mission for robot"}
+
+        _excute_task_code = ["transport_empty_cart", "transport_goods"]
         if _excute_code == "transport_empty_cart":
             mission_infor = self.mission_transport_empty_cart_current
-            # mission_infor = None
-        elif _excute_code == "transport_goods":
-            # mission_infor = None
-            mission_infor = self.mission_transport_goods_current
 
-        # mission_infor = None
-        # url_name = "missions_excute_pop"
-        # _location = {"excute_code": _excute_code}
-        # data_return = self.processing_update_client(str(url_name), _location)
-        if mission_infor is None:
-            _map_code.remove(_excute_code)
-            return self.robot_processing_take_mission(_map_code[0], n - 1)
-            # self.get_logger().info('request_robot: "%s"' % (_map_code[0]))
-        # self.get_logger().info('mission_infor: "%s"' % (mission_infor))
-        # self.get_logger().info('_excute_code: "%s"' % (_excute_code))
-        # self.get_logger().info('n: "%s"' % (n))
+        elif _excute_code == "transport_goods":
+
+            mission_infor = self.mission_transport_goods_current
+        else:
+            return {"mission_code": 0, "code": "type work not exist"}
+
+        if not mission_infor["mission_code"]:
+            _excute_task_code.remove(_excute_code)
+            return self.robot_processing_take_mission(_excute_task_code[0], n - 1)
 
         _location = {"excute_code": _excute_code}
         url_name = "missions_excute_pop"
         data_return = self.processing_update_client(str(url_name), _location)
+
         return mission_infor
 
     def mission_current_callback(self, msg):
-        _mission_transport_goods_current = eval(msg.transport_goods)
-        _mission_transport_empty_cart_current = eval(msg.transport_empty_cart)
-        if _mission_transport_goods_current["code"]:
-            self.mission_transport_goods_current = {
-                "entry_location": _mission_transport_goods_current["entry_location"],
-                "end_location": _mission_transport_goods_current["end_location"],
-                "excute_code": "transport_goods",
-            }
-        self.mission_transport_empty_cart_current = {
-            "entry_location": _mission_transport_empty_cart_current["entry_location"],
-            "end_location": _mission_transport_empty_cart_current["end_location"],
-            "excute_code": "transport_empty_cart",
-        }
-        # self.get_logger().info(
-        #     'request_body: "%s"' % self.mission_transport_goods_current
-        # )
+
+        _mission_transport_goods_current = eval(msg.data)
 
     def cart_empty_callback(self, msg):
         # self.get_logger().info("hello on here")
@@ -422,9 +430,9 @@ def main(args=None):
     # executor.spin()
     spin_thread = threading.Thread(target=executor.spin, daemon=True)
     spin_thread.start()
-    minimal_client.get_logger().info("Swagger run http://192.168.68.110:2100/docs#/")
+    minimal_client.get_logger().info("Swagger run http://127.0.0.0:2100/docs#/")
 
-    uvicorn.run(app, host="192.168.68.110", port=2100, log_level="warning")
+    uvicorn.run(app, host="0.0.0.0", port=2100, log_level="warning")
     minimal_client.destroy_node()
 
     rclpy.shutdown()
